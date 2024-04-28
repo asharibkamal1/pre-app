265350997a0c80c705458153b296373064354fb0
asharib.kamal <asharib.kamal@pral.com.pk>
2024-04-28 23:46:13 +0500

    c

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
         <h1 className='mb-4'>Assalam o alikum</h1>
-        <p>My First React App + Vite +asharib kamal and areeba rahim</p>
+        <p>My First React App + Vite + Type Script</p>
     </>
   )
 }
