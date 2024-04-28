b313ed12c0b8925dca5c52280691aa4423ec4881
asharib.kamal <asharib.kamal@pral.com.pk>
2024-04-28 23:37:25 +0500

    change in paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
         <h1 className='mb-4'>Assalam o alikum</h1>
-        <p>asharib kamal and areeba rahim</p>
+        <p>My First React App + Vite + TypeScript <br>asharib kamal and areeba rahim</br></p>
     </>
   )
 }
