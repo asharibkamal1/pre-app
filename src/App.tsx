40f6cb961bb98ae4b1f3013dfd8fa9ee1c8a3ded
asharib.kamal <asharib.kamal@pral.com.pk>
2024-06-20 14:03:50 +0500

    as

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
   return (
     <>
-        <h1 className='mb-4'>Assalam o alikum</h1>
-        <p>My First React App + Vite + Type Script</p>
+        <h1 className='mb-6'>Assalam o alikum  </h1>
+        <p className='bg-red-500'>My First React App + Vite Asharib kamal + Type Script</p>
     </>
   )
 }
